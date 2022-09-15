7760eb231299c4aa1d7a390513c451356f19179c
Leandro Pereira <103958713+OLeandroPereira@users.noreply.github.com>
2022-09-14 23:04:47 -0300

    footer e navegações

--- Churrascaria/js/script.js
+++ Churrascaria/js/script.js
@@ -64,6 +64,45 @@ $('.fotos-churrascaria').slick({
 });
 
 
+//FIXAR O MENU DE NAVEGAÇÃO NO TOPO DA PÁGINA
+$(window).scroll(function(){
+  let position = $(this).scrollTop();
+  
+  
+  if(position >= 630){
+    $('.navbar').addClass('navbar-background');
+    $('.navbar').addClass('fixed-top');
+  }else{
+    $('.navbar').removeClass('navbar-background');
+    $('.navbar').removeClass('fixed-top');
+  }
+});
 
+
+
+
+//BOTÃO VOLTAR AO TOPO
+  $(window).scroll(function(){
+    let position = $(this).scrollTop();
+
+    if(position >= 680){
+      $('#voltar-topo').addClass('scrollTop');
+    }else{
+      $('#voltar-topo').removeClass('scrollTop');
+    }  
+    
+});
+
+
+//SUAVIZA SCROLL PARA NAVEGAÇÃO
+$('.nav-item a, header-link, .btn-adiante, #voltar-topo, .navbar-brand').click(function(link){
+    link.preventDefault();
+    let target = $(this).attr('href');
+
+    $('html, body').stop().animate({
+      scrollTop: $(target).offset().top -25
+    }, 1000);  
+});
 
 })
+
